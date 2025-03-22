3a56563424991172476a285615213de778540a58
ashiques-dev <ashiques.dev@gmail.com>
2025-03-22 18:37:27 +0530

    maintanance mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
   return (
     <>
-    
+      <Image
+        src={"/maintanace.webp"}
+        alt="maintanace image"
+        height={1080}
+        width={720}
+        className="size-full object-cover object-center"
+      />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
